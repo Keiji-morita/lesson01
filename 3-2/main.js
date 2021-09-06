0288079bf4f5a5356ebc9e36145bb2aba2e1004d
Keiji-morita <keijim3116@gmail.com>
2021-09-06 16:28:04 +0900

    3-2解き直しです

--- 3-2/main.js
+++ 3-2/main.js
@@ -1,14 +1,25 @@
-const tatelines = process.argv[2]
-const yokolines = process.argv[3]
+const tatelines=process.argv[2];
+const yokolines=process.argv[3];
 
-
-for(let i = 1; i < tatelines*yokolines; i++){
-  if(i % 2 === 0){
-    process.stdout.write("-")
-  }else{
-    process.stdout.write("*")
+for(let i = 1; i <= tatelines; i++){
+  let array = [];
+   if(i % 2 === 1){
+     for(let j = 1; j <= yokolines; j++){
+       if(j % 2 === 1){
+        array.push("*");
+       }else{
+         array.push("-");
+       }
+     }
+   }else{
+     for(let j = 1; j <= yokolines; j++){
+       if(j % 2 === 1){
+        array.push("-")
+       }else{ 
+        array.push("*")
+        }
+       }
+     }
+   console.log(array.join(""))
+  
   }
-  if(i % yokolines === 0){
-    console.log(" ")
-  }
-};
