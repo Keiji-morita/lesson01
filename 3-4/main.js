e86486279eb4133637381857496e199b53b76131
Keiji-morita <keijim3116@gmail.com>
2021-09-10 12:31:32 +0900

    3-4問題です

---
+++ 3-4/main.js
@@ -0,0 +1,14 @@
+const number1 = process.argv[2];
+const number2 = process.argv[3];
+
+const reversed = number1.split('')    
+                     .reverse()    
+                     .join('');    
+   
+
+const revresed2 = number2.split('')
+                     .reverse()
+                     .join('');
+
+
+console.log(Number(reversed) + Number(revresed2));
